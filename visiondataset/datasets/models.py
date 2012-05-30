ce96f27aeecf443df0eceb2a7448579f61828794
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-05-30 14:45:31 -0300

    lints

--- visiondataset/datasets/models.py
+++ visiondataset/datasets/models.py
@@ -6,10 +6,11 @@ from django_extensions.db.fields import CreationDateTimeField, ModificationDateT
 
 class Dataset(models.Model):
     """Set of datums"""
-    name = models.CharField(max_length=256, blank=False)
+    name = models.CharField(max_length=256)
     slug = AutoSlugField(_('slug'), populate_from='name')
-    owner = models.ForeignKey(User, null=False)
+    owner = models.ForeignKey(User, related_name='+')
     created = CreationDateTimeField(_('created'))
+
     class Meta:
         permissions = (
             ('view_dataset', _('View Dataset')),
@@ -17,44 +18,50 @@ class Dataset(models.Model):
         )
         get_latest_by = 'created'
         ordering = ('-created',)
-    @models.permalink
-    def get_absolute_url(self):
-        return ('dataset.views.datatype',(),{'slug':str(self.slug)})
+
     def __unicode__(self):
         return self.name
 
+    @models.permalink
+    def get_absolute_url(self):
+        return ('dataset.views.datatype',(),{'slug':str(self.slug)})
 
 
 class DataType(models.Model):
     """docstring"""
-    name = models.CharField(max_length=50, blank=False)
+    name = models.CharField(max_length=50)
     slug = AutoSlugField(_('slug'), populate_from='name')
+
+    def __unicode__(self):
+        return self.name
+
     @models.permalink
     def get_absolute_url(self):
         return ('dataset.views.datatype',(),{'slug':str(self.slug)})
-    def __unicode__(self):
-        return self.name
 
 
 class Datum(models.Model):
     """Data element"""
-    dataset = models.ForeignKey(Dataset, null=False)
-    owner = models.ForeignKey(User, null=False)
+    dataset = models.ForeignKey(Dataset, related_name='+')
+    owner = models.ForeignKey(User, related_name='+')
     created = CreationDateTimeField(_('created'))
-    name = models.CharField(max_length=256, blank=False)
+    name = models.CharField(max_length=256)
     slug = AutoSlugField(_('slug'), populate_from='name')
-    description = models.TextField(_('description'), blank=True, null=True)
+    description = models.TextField(_('description'), blank=True)
     package = models.FileField(upload_to='datum', max_length=100)
-    dtype = models.ForeignKey(DataType)
+    dtype = models.ForeignKey(DataType, related_name='+')
+
     class Meta:
         permissions = (
             ('view_datum', _('View Element Datum')),
         )
         get_latest_by = 'created'
         ordering = ('dataset','-created')
+
+    def __unicode__(self):
+        return self.name
+
     @models.permalink
     def get_absolute_url(self):
         return ('dataset.views.datatype',(),{'slug':str(self.slug)})
-    def __unicode__(self):
-        return self.name
 
