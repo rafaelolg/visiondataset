aaf389fb93a6bb051b8da0bfd90370b742dbc529
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-06-02 00:56:07 -0300

    Dataset model form

---
+++ visiondataset/datasets/forms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm
+from models import Dataset
+
+
+class DatasetModelForm(ModelForm):
+    """Form for Dataset"""
+    class Meta:
+        model = Dataset
+        exclude = ('owner', 'created', 'slug')
+
+# forms.py
+
