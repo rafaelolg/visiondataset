18dcfdcdd7773cd52823c46cbf5383f17e704048
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-06-19 19:35:21 -0300

    Test for models.

--- visiondataset/datasets/tests.py
+++ visiondataset/datasets/tests.py
@@ -7,11 +7,46 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from .models import *
+from django.contrib.auth.models import User
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        print('asdasdasdasd')
-        self.assertEqual(1 + 1, 2)
+class TestDatasetModel(TestCase):
+    fixtures = ['test_data.json',]
+    def setUp(self):
+        self.d1 = Dataset.objects.get(name='d1')
+        self.d2 = Dataset.objects.get(name='d2')
+        self.dd1 = Datum.objects.get(name='dd1.png')
+        self.dd2 = Datum.objects.get(name='dd2.png')
+        self.a1 = DatumAttachment.objects.get(name='a1')
+        self.a2 = DatumAttachment.objects.get(name='a2')
+        self.u1 = User.objects.get(username='u1')
+        self.u2 = User.objects.get(username='u2')
+        self.imgdt = DataType.objects.get(name='image')
+
+
+    def test_dataset_is_user_allowed(self):
+        self.assertTrue(self.d1.is_user_allowed(self.u1))
+        self.assertTrue(self.d1.is_user_allowed(self.u2))
+        self.assertTrue(self.dd1.is_user_allowed(self.u1))
+        self.assertTrue(self.dd1.is_user_allowed(self.u2))
+        self.assertTrue(self.a1.is_user_allowed(self.u1))
+        self.assertTrue(self.a1.is_user_allowed(self.u2))
+
+        self.assertTrue(self.d2.is_user_allowed(self.u2))
+        self.assertTrue(self.dd2.is_user_allowed(self.u2))
+        self.assertTrue(self.a2.is_user_allowed(self.u2))
+
+    def test_user_not_allowed(self):
+        self.assertFalse(self.d2.is_user_allowed(self.u1))
+        self.assertFalse(self.dd2.is_user_allowed(self.u1))
+        self.assertFalse(self.a2.is_user_allowed(self.u1))
+
+
+
+    def test_datasetcolaborators(self):
+        self.assertEquals(self.d1.colaborators().count(), 2)
+        self.assertEquals(self.d2.colaborators().count(), 1)
+
+    def test_typeaccept(self):
+        self.assertTrue(self.imgdt.is_acceptable('test.png'))
+        self.assertFalse(self.imgdt.is_acceptable('test.avi'))
