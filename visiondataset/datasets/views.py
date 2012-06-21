4bb07c1edd5ba84f371a2afc5ca46f76e0af9c21
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-06-21 19:34:13 -0300

    Just reordering things in views.

--- visiondataset/datasets/views.py
+++ visiondataset/datasets/views.py
@@ -30,7 +30,7 @@ from util import base_name
 
 DATASET_COLABORATE_PERMISSION = 'dataset_colaborate'
 
-
+##################DETAIL AND LISDT
 class DatasetList(LoginRequiredMixin, ListView):
 
     context_object_name = 'dataset_list'
@@ -46,19 +46,6 @@ class DatasetList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super(DatasetList, self).get_context_data(**kwargs)
         return context
-
-
-class DatasetCreate(LoginRequiredMixin, CreateView):
-
-    model = Dataset
-    form_class = DatasetModelForm
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.owner = self.request.user
-        self.object.save()
-        assign(DATASET_COLABORATE_PERMISSION, self.request.user, self.object)
-        return HttpResponseRedirect(self.get_success_url())
 
 
 class DatasetDetail(PermissionRequiredMixin, LoginRequiredMixin, ListView):
@@ -131,6 +118,67 @@ class DatumAttachmentDetail(LoginRequiredMixin, DetailView):
         return context
 
 
+################ GET FILES
+@login_required
+def datum_attachment_file(request, pk, datum_id=None, dataset_id=None):
+    datum = get_object_or_404(Datum, pk=datum_id)
+    attachment = get_object_or_404(DatumAttachment, pk=pk)
+    filename = attachment.original_name
+    absolut_file_path = attachment.package.path
+    return datum_send_file(request, filename, absolut_file_path, datum)
+
+
+@login_required
+def datum_file(request, pk, dataset_id=None):
+    datum = get_object_or_404(Datum, pk=pk)
+    filename = datum.file_name()
+    absolut_file_path = datum.package.path
+    return datum_send_file(request, filename, absolut_file_path, datum)
+
+
+@login_required
+def datum_thumbnail(request, pk, dataset_id=None):
+    datum = get_object_or_404(Datum, pk=pk)
+    filename = 'thumb_' + datum.file_name()
+    absolut_file_path = datum.get_thumbnail_file_path()
+    return datum_send_file(request, filename, absolut_file_path, datum)
+
+
+@login_required
+def datum_send_file(request, filename, absolut_file_path, datum,):
+    if datum.is_user_allowed(request.user):
+        return sendfile(request, absolut_file_path, attachment=True,
+                        attachment_filename=filename)
+    else:
+        return HttpResponseForbidden(_("You can't view this"))
+
+
+@login_required
+def dataset_as_zip(request, pk):
+    dataset = get_object_or_404(Dataset, pk=pk)
+    if not request.user.has_perm('dataset_colaborate', dataset):
+        return HttpResponseForbidden(_("You can't view this"))
+    response = HttpResponse(mimetype='application/zip')
+    response['Content-Disposition'] = 'attachment; filename=%s.zip'\
+         % slugify(dataset.name)
+    response.write(dataset.to_zip().read())
+    return response
+
+
+####################CREATE
+class DatasetCreate(LoginRequiredMixin, CreateView):
+
+    model = Dataset
+    form_class = DatasetModelForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.owner = self.request.user
+        self.object.save()
+        assign(DATASET_COLABORATE_PERMISSION, self.request.user, self.object)
+        return HttpResponseRedirect(self.get_success_url())
+
+
 class DatumCreate(LoginRequiredMixin, CreateView):
 
     model = Datum
@@ -177,88 +225,6 @@ class DatumCreate(LoginRequiredMixin, CreateView):
         return HttpResponseRedirect(dataset.get_absolute_url())
 
 
-@login_required
-def datum_attachment_file(request, pk, datum_id=None, dataset_id=None):
-    datum = get_object_or_404(Datum, pk=datum_id)
-    attachment = get_object_or_404(DatumAttachment, pk=pk)
-    filename = attachment.original_name
-    absolut_file_path = attachment.package.path
-    return datum_send_file(request, filename, absolut_file_path, datum)
-
-
-@login_required
-def datum_file(request, pk, dataset_id=None):
-    datum = get_object_or_404(Datum, pk=pk)
-    filename = datum.file_name()
-    absolut_file_path = datum.package.path
-    return datum_send_file(request, filename, absolut_file_path, datum)
-
-
-@login_required
-def datum_thumbnail(request, pk, dataset_id=None):
-    datum = get_object_or_404(Datum, pk=pk)
-    filename = 'thumb_' + datum.file_name()
-    absolut_file_path = datum.get_thumbnail_file_path()
-    return datum_send_file(request, filename, absolut_file_path, datum)
-
-
-@login_required
-def datum_send_file(request, filename, absolut_file_path, datum,):
-    if datum.is_user_allowed(request.user):
-        return sendfile(request, absolut_file_path, attachment=True,
-                        attachment_filename=filename)
-    else:
-        return HttpResponseForbidden(_("You can't view this"))
-
-
-@login_required
-def edit_colaborators(request, dataset_id):
-    dataset = get_object_or_404(Dataset, pk=dataset_id)
-
-    if request.user != dataset.owner:
-        return HttpResponseForbidden(_("You can't view this"))
-
-    if request.method == 'POST':
-        form = ColaboratorForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            try:
-                u = User.objects.get(username=username)
-            except Exception, e:
-                messages.error(request, _('No such user: ') + username)
-            assign(DATASET_COLABORATE_PERMISSION, u, dataset)
-            form = ColaboratorForm()
-            messages.success(request, _('New colaborator: ') + username)
-    else:
-        form = ColaboratorForm()
-    return render_to_response('datasets/dataset_colaborators.html',
-            {'form': form, 'dataset': dataset},
-            context_instance=RequestContext(request))
-    return HttpResponseForbidden()
-
-
-@login_required
-def remove_colaborators(request, dataset_id, colaborator_id):
-    dataset = get_object_or_404(Dataset, pk=dataset_id)
-    if request.user != dataset.owner or colaborator_id == dataset.owner_id:
-        return HttpResponseForbidden(_("You can't view this"))
-    colaborator = get_object_or_404(User, pk=colaborator_id)
-    remove_perm(DATASET_COLABORATE_PERMISSION, colaborator, dataset)
-    return redirect('datasets_dataset_colaborators', dataset_id=dataset_id)
-
-
-@login_required
-def dataset_as_zip(request, pk):
-    dataset = get_object_or_404(Dataset, pk=pk)
-    if not request.user.has_perm('dataset_colaborate', dataset):
-        return HttpResponseForbidden(_("You can't view this"))
-    response = HttpResponse(mimetype='application/zip')
-    response['Content-Disposition'] = 'attachment; filename=%s.zip'\
-         % slugify(dataset.name)
-    response.write(dataset.to_zip().read())
-    return response
-
-
 class DatumAttachmentCreate(LoginRequiredMixin, CreateView):
 
     model = DatumAttachment
@@ -287,3 +253,41 @@ class DatumAttachmentCreate(LoginRequiredMixin, CreateView):
         self.object.original_name = self.object.package.name
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
+
+
+##################DELETE
+@login_required
+def remove_colaborators(request, dataset_id, colaborator_id):
+    dataset = get_object_or_404(Dataset, pk=dataset_id)
+    if request.user != dataset.owner or colaborator_id == dataset.owner_id:
+        return HttpResponseForbidden(_("You can't view this"))
+    colaborator = get_object_or_404(User, pk=colaborator_id)
+    remove_perm(DATASET_COLABORATE_PERMISSION, colaborator, dataset)
+    return redirect('datasets_dataset_colaborators', dataset_id=dataset_id)
+
+
+##################EDIT
+@login_required
+def edit_colaborators(request, dataset_id):
+    dataset = get_object_or_404(Dataset, pk=dataset_id)
+
+    if request.user != dataset.owner:
+        return HttpResponseForbidden(_("You can't view this"))
+
+    if request.method == 'POST':
+        form = ColaboratorForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            try:
+                u = User.objects.get(username=username)
+            except Exception, e:
+                messages.error(request, _('No such user: ') + username)
+            assign(DATASET_COLABORATE_PERMISSION, u, dataset)
+            form = ColaboratorForm()
+            messages.success(request, _('New colaborator: ') + username)
+    else:
+        form = ColaboratorForm()
+    return render_to_response('datasets/dataset_colaborators.html',
+            {'form': form, 'dataset': dataset},
+            context_instance=RequestContext(request))
+    return HttpResponseForbidden()
