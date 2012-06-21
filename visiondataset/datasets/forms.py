d7a30c413893e285e7c297fc43f77cb4f8062eff
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-06-21 15:17:00 -0300

    pep8

--- visiondataset/datasets/forms.py
+++ visiondataset/datasets/forms.py
@@ -1,39 +1,52 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from django.forms import ModelForm
-from models import Dataset, Datum, DatumAttachment
+
 from django import forms
+from django.forms import ModelForm
 from django.forms import fields, models, formsets, widgets
 from django.utils.translation import ugettext_lazy as _
 
+from models import Dataset, Datum, DatumAttachment
+
+
 class DatasetModelForm(ModelForm):
-    """Form for Dataset"""
+    """
+    Form for Dataset
+    """
+
     class Meta:
         model = Dataset
         exclude = ('owner', 'created')
 
+
 class DatumModelForm(ModelForm):
-    initial={'dtype': 1}
+
+    initial = {'dtype': 1}
     class Meta:
         model = Datum
         exclude = ('name', 'owner', 'dataset', 'created')
 
 
 class DatumAttachmentForm(ModelForm):
+
     class Meta:
         model = DatumAttachment
         exclude = ('owner', 'datum', 'created', 'original_name')
 
+
 class ColaboratorForm(forms.Form):
-    username = forms.CharField(max_length=256, required=True, label=_('User or Email'))
+
+    username = forms.CharField(max_length=256, required=True,
+                               label=_('User or Email'))
 
     def __init__(self, *args, **kwargs):
         super(ColaboratorForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget = widgets.TextInput(attrs={'class': 'autocomplete-me'})
+        self.fields['username'].widget = \
+            widgets.TextInput(attrs={'class': 'autocomplete-me'})
 
     class Media:
-        js = ('js/libs/jquery.autocomplete.min.js', 'js/libs/autocomplete-init.js',)
-        css = {
-            'all': ('css/jquery.autocomplete.css',),
-        }
+        js = ('js/libs/jquery.autocomplete.min.js',
+              'js/libs/autocomplete-init.js')
+        css = {'all': ('css/jquery.autocomplete.css', )}
+
 
