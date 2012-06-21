b3a361e3c49f11bcbe35805155be0169897f1af6
Rafael Lopes <rafaellg@vision.ime.usp.br>
2012-06-21 15:21:54 -0300

    pep8

--- visiondataset/datasets/admin.py
+++ visiondataset/datasets/admin.py
@@ -1,33 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# admin.py
+
+
 from django.contrib import admin
 from guardian.admin import GuardedModelAdmin
+
 from models import Dataset, Datum, DataType, DatumAttachment
 
+
 class DatasetAdmin(GuardedModelAdmin):
-    list_display = ('name' , 'owner', 'created')
+
+    list_display = ('name', 'owner', 'created')
     search_fields = ['user__name']
 
 
 class DataTypeAdmin(GuardedModelAdmin):
+
     pass
 
+
 class DatumAttachmentAdmin(GuardedModelAdmin):
-    list_display = ('name' , 'datum', 'owner', 'created')
+
+    list_display = ('name', 'datum', 'owner', 'created')
+
 
 class DatumAttachmentInlineAdmin(admin.TabularInline):
-    model=DatumAttachment
+
+    model = DatumAttachment
     extra = 1
 
 
 class DatumAdmin(GuardedModelAdmin):
-    list_display = ('name' , 'dataset','owner', 'created')
-    inlines = [ DatumAttachmentInlineAdmin, ]
+
+    list_display = ('name', 'dataset', 'owner', 'created')
+    inlines = [DatumAttachmentInlineAdmin]
+
 
 admin.site.register(Datum, DatumAdmin)
 admin.site.register(DatumAttachment, DatumAttachmentAdmin)
 admin.site.register(Dataset, DatasetAdmin)
 admin.site.register(DataType, DataTypeAdmin)
-
-
-# admin.py
